a8b40b4c9c7c92c920cd3ebd6d186f07b0969c82
yilma <yilma@DESKTOP-U1LH654>
2023-05-26 18:59:59 +0300

    Add project files.

---
+++ 262523_MvcGiris/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _262523_MvcGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Meetings()
+        {
+            return View();
+        }
+        public ActionResult Apply()
+        {
+            return View();
+        }
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 262523_MvcGiris/Controllers/PagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _262523_MvcGiris.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Meeting()
+        {
+            return View();
+        }
+        public ActionResult MeetingDetail()
+        {
+            return View();
+        }
+    }
+}
